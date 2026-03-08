d631894cf06942da9f91aa1a3a9f964ccc4bae55
FaisalVanu30 <faisal.221759.cs@mhssce.ac.in>
2026-03-08 22:34:45 +0530

    fix: increase upload size limit

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ connectDB();
    MIDDLEWARE
    ───────────────────────────────────────────── */
 app.use(cors());
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 /* ─────────────────────────────────────────────
    STATIC FILES
